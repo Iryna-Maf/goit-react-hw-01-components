7bc27fd5f842f01f661c51354b71fc6f7a34fa83
Iryna-Maf <100190659+Iryna-Maf@users.noreply.github.com>
2022-08-15 17:54:12 +0300

    done 1,2,3,4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
+import TransactionHistory from './Transaction/TransactionHistory';
+import user from '../components/Data/user.json';
+import data from '../components/Data/data.json';
+import friends from '../components/Data/friends.json';
+import transactions from '../components/Data/transactions.json';
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+    // style={{
+    //   height: '100vh',
+    //   display: 'flex',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   fontSize: 40,
+    //   color: '#010101',
+    // }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
